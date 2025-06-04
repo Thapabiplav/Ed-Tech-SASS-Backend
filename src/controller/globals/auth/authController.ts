baf3c773ce5fb63606bd1649dde2fff6bba80f46
Birat <thapabirat95@gmail.com>
2025-06-04 22:29:15 +0545

    added login feature

--- src/controller/globals/auth/authController.ts
+++ src/controller/globals/auth/authController.ts
@@ -26,6 +26,43 @@ class AuthController {
       message: "user registered successfully",
     });
   };
+
+  static loginUser = async (req: Request, res: Response) => {
+    if (!req.body) {
+      res.status(400).json({
+        message: "No data was sent",
+      });
+      return;
+    }
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        message: "please provide above details",
+      });
+      return;
+    }
+    const [data] = await User.findAll({
+      where: {
+        email,
+      },
+    });
+    if (!data) {
+      res.status(400).json({
+        message: "above email is not registered",
+      });
+      return;
+    }
+    const isMatched = bcrypt.compareSync(password, data.password);
+    if (!isMatched) {
+      res.status(400).json({
+        message: "Invalid email or password",
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "login successfully",
+    });
+  };
 }
 
 export default AuthController;
